997d625301fa31d7308c7592984752e69c04e970
Alexey Karabanov <thewyolar@gmail.com>
2022-04-09 10:24:48 +0300

    Add icon, dictionary.

--- src/main/java/com/wordle/MainApplication.java
+++ src/main/java/com/wordle/MainApplication.java
@@ -3,21 +3,38 @@ package com.wordle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class MainApplication extends Application {
+
+    public static final ArrayList<String> dictionaryWords = new ArrayList<>();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Wordle");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/icon.png"))));
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void initializeWordList() {
+        InputStream dictionary = getClass().getResourceAsStream("dictionary.txt");
+        Stream<String> dictionary_lines = new BufferedReader(new InputStreamReader(dictionary)).lines();
+        dictionary_lines.forEach(dictionaryWords::add);
     }
 }
