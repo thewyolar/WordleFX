73ff11bd7b2061246771956cee6d95e69f7ee644
Alexey Karabanov <thewyolar@gmail.com>
2022-04-17 22:15:19 +0300

    Update
    Add timer.

--- src/main/java/com/wordle/controller/ResultController.java
+++ src/main/java/com/wordle/controller/ResultController.java
@@ -2,10 +2,17 @@ package com.wordle.controller;
 
 import com.wordle.MainApplication;
 import com.wordle.model.MainHandler;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ResultController {
 
@@ -17,6 +24,22 @@ public class ResultController {
 
     @FXML
     private Label winningWordLabel;
+
+    @FXML
+    private Text timer;
+
+    @FXML
+    public void initialize() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(1),
+                        e -> {
+                            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                            timer.setText(dateTimeFormatter.format(LocalDateTime.now()));
+                        }));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
 
     public BorderPane getBorderPane() { return borderPane; }
 
