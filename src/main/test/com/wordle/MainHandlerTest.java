55c7ed2729f36b8dbc06b69495a5641aa066f99f
Alexey Karabanov <thewyolar@gmail.com>
2022-06-12 17:17:41 +0300

    Update unit tests

--- src/main/test/com/wordle/MainHandlerTest.java
+++ src/main/test/com/wordle/MainHandlerTest.java
@@ -14,11 +14,15 @@ class MainHandlerTest {
     void binarySearchTest() {
         ArrayList<String> testDictionary = new ArrayList<>(Arrays.asList("абзац", "пирог", "сапер", "север"));
         assertEquals(true, testMainHandler.binarySearch(testDictionary, "север"));
+        assertEquals(false, testMainHandler.binarySearch(testDictionary, "сапог"));
+        assertEquals(true, testMainHandler.binarySearch(testDictionary, "абзац"));
     }
 
     @Test
     void containsTest() {
         String[] testArray = new String[] { "носок", "сапог", "обвал" };
         assertEquals(true, testMainHandler.contains(testArray, "обвал"));
+        assertEquals(false, testMainHandler.contains(testArray, "пирог"));
+        assertEquals(true, testMainHandler.contains(testArray, "носок"));
     }
 }
